07a980ecbf9a2abc737692b003022ec29817535c
Nathan Wang <nathan.r.wang@gmail.com>
2019-02-02 10:58:36 -0800

    codeforces 1038C - Gambling

---
+++ codeforces/1038C - Gambling.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <unordered_set>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F0R1(i, a) for (int i=1; i<=(a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define F0Rd1(i, a) for (int i=a; i>0; i--)
+#define SORT(vec) sort(vec.begin(), vec.end())
+
+#define INF 1000000010
+#define LL_INF 4500000000000000000
+#define LSOne(S) (S & (-S))
+#define EPS 1e-9
+#define pA first
+#define pB second
+#define mp make_pair
+#define pb push_back
+#define PI acos(-1.0)
+#define ll long long
+#define MOD (int)(2e+9+11)
+#define MOD2 1000000007
+#define SET(vec, val, size) for (int i = 0; i < size; i++) vec[i] = val;
+#define SET2D(arr, val, dim1, dim2) F0R(i, dim1) F0R(j, dim2) arr[i][j] = val;
+#define SET3D(arr, val, dim1, dim2, dim3) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) arr[i][j][k] = val;
+#define SET4D(arr, val, dim1, dim2, dim3, dim4) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) F0R(l, dim4) arr[i][j][k][l] = val;
+#define READGRID(arr, dim) F0R(i, dim) F0R(j, dim) cin >> arr[i][j];
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<ll> vl;
+
+void setupIO(const string &PROB) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ifstream infile(PROB + ".in");
+    if (infile.good()) {
+        freopen((PROB + ".in").c_str(), "r", stdin);
+        freopen((PROB + ".out").c_str(), "w", stdout);
+    }
+}
+
+/* ============================ */
+
+int main() {
+    int n; cin >> n;
+    vector<ll> A, B;
+    F0R(i, n) {
+        ll x; cin >> x;
+        A.pb(x);
+    }
+    F0R(i, n) {
+        ll x; cin >> x;
+        B.pb(x);
+    }
+    SORT(A);
+    SORT(B);
+    ll a = 0, b = 0;
+    bool aTurn = true;
+    while (!(A.empty() && B.empty())) {
+        ll opA = A.empty() ? 0 : A.back();
+        ll opB = B.empty() ? 0 : B.back();
+        if (opA > opB) {
+            if (aTurn) a += opA;
+            A.pop_back();
+        } else {
+            if (!aTurn) b += opB;
+            B.pop_back();
+        }
+        aTurn = !aTurn;
+    }
+    cout << a - b << endl;
+
+    return 0;
+}
