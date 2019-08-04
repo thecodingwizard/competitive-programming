fe1985fcea438d01172fcd01758c06e3bee1a138
Nathan Wang <nathan.r.wang@gmail.com>
2019-08-04 12:45:00 -0700

    1201D

---
+++ Codeforces/1201C - Maximum Median.cpp
@@ -0,0 +1,185 @@
+//#pragma GCC optimize ("O3")
+//#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F0R1(i, a) for (int i=1; i<=(a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define MIN(a, b) a = min(a, b)
+#define MAX(a, b) a = max(a, b)
+
+#define INF 1000000010
+#define LL_INF 4500000000000000000LL
+#define LSOne(S) (S & (-S))
+#define EPS 1e-9
+#define pA first
+#define pB second
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define PI acos(-1.0)
+// #define MOD (int)(2e+9+11)
+#define MOD (int)(1e+9+7)
+#define SET(vec, val, size) for (int i = 0; i < size; i++) vec[i] = val;
+#define SET2D(arr, val, dim1, dim2) F0R(i, dim1) F0R(j, dim2) arr[i][j] = val;
+#define SET3D(arr, val, dim1, dim2, dim3) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) arr[i][j][k] = val;
+#define SET4D(arr, val, dim1, dim2, dim3, dim4) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) F0R(l, dim4) arr[i][j][k][l] = val;
+
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+#define SORT(vec) sort(all(vec))
+#define RSORT(vec) sort(vec.rbegin(),vec.rend())
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<ll> vl;
+
+// @formatter:off
+// Source: Benq (https://github.com/bqi343/USACO) [Modified]
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+    template<class T> void reA(T A[], int sz);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) {
+        re(first); re(rest...);
+    }
+
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.pA,p.pB); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+    template<class T> void reA(T A[], int sz) { F0R(i, sz) re(A[i]); }
+
+    void setupIO(const string &PROB = "") {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        if (PROB.length() != 0) {
+            ifstream infile(PROB + ".in");
+            if (infile.good()) {
+                freopen((PROB + ".in").c_str(), "r", stdin);
+                freopen((PROB + ".out").c_str(), "w", stdout);
+            }
+        }
+    }
+}
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void prD(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x);
+    template<class T> void prD(const vector<T>& x);
+    template<class T> void prD(const set<T>& x);
+    template<class T1, class T2> void prD(const map<T1,T2>& x);
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void prD(const T& x) { cout << x; }
+    template<class Arg, class... Args> void prD(const Arg& first, const Args&... rest) {
+        prD(first); prD(rest...);
+    }
+
+    template<class T1, class T2> void prD(const pair<T1,T2>& x) {
+        prD("{",x.pA,", ",x.pB,"}");
+    }
+    template<class T> void prDContain(const T& x) {
+        prD("{");
+        bool fst = 1; for (const auto& a: x) prD(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        prD("}");
+    }
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x) { prDContain(x); }
+    template<class T> void prD(const vector<T>& x) { prDContain(x); }
+    template<class T> void prD(const set<T>& x) { prDContain(x); }
+    template<class T1, class T2> void prD(const map<T1,T2>& x) { prDContain(x); }
+
+    void psD() { prD("\n"); }
+    template<class Arg> void psD(const Arg& first) {
+        prD(first); psD(); // no space at end of line
+    }
+    template<class Arg, class... Args> void psD(const Arg& first, const Args&... rest) {
+        prD(first," "); psD(rest...); // print w/ spaces
+    }
+
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+        pr(first); pr(rest...);
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+        pr(x.pA, " ", x.pB);
+    }
+    template<class T> void prContain(const T& x) {
+        bool fst = 1; for (const auto& a: x) pr(!fst?" ":"",a), fst = 0; // const needed for vector<bool>
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) {
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+using namespace output;
+// @formatter:on
+
+/* ============================ */
+
+int main() {
+    setupIO();
+
+    int n, k; re(n, k);
+    int A[n]; reA(A, n);
+    sort(A, A+n);
+
+    ll k2 = 0;
+    ll ans = A[n/2];
+    ll ctr = 0;
+    FOR(i, n/2, n-1) {
+        ctr++;
+        ll cur = A[i], nxt = A[i + 1];
+        if ((nxt - cur)*ctr > k - k2) {
+            ans = cur + (k - k2)/ctr;
+            k2 = k;
+            break;
+        }
+        k2 += (nxt - cur)*ctr;
+        ans = nxt;
+    }
+    ps(ans+(k-k2)/(++ctr));
+
+    return 0;
+}
---
+++ Codeforces/1201D - Treasure Hunting.cpp
@@ -0,0 +1,264 @@
+//#pragma GCC optimize ("O3")
+//#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F0R1(i, a) for (int i=1; i<=(a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define MIN(a, b) a = min(a, b)
+#define MAX(a, b) a = max(a, b)
+
+#define INF 1000000010
+#define LL_INF 4500000000000000000LL
+#define LSOne(S) (S & (-S))
+#define EPS 1e-9
+#define pA first
+#define pB second
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define PI acos(-1.0)
+// #define MOD (int)(2e+9+11)
+#define MOD (int)(1e+9+7)
+#define SET(vec, val, size) for (int i = 0; i < size; i++) vec[i] = val;
+#define SET2D(arr, val, dim1, dim2) F0R(i, dim1) F0R(j, dim2) arr[i][j] = val;
+#define SET3D(arr, val, dim1, dim2, dim3) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) arr[i][j][k] = val;
+#define SET4D(arr, val, dim1, dim2, dim3, dim4) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) F0R(l, dim4) arr[i][j][k][l] = val;
+
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+#define SORT(vec) sort(all(vec))
+#define RSORT(vec) sort(vec.rbegin(),vec.rend())
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<ll> vl;
+
+// @formatter:off
+// Source: Benq (https://github.com/bqi343/USACO) [Modified]
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+    template<class T> void reA(T A[], int sz);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) {
+        re(first); re(rest...);
+    }
+
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.pA,p.pB); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+    template<class T> void reA(T A[], int sz) { F0R(i, sz) re(A[i]); }
+
+    void setupIO(const string &PROB = "") {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        if (PROB.length() != 0) {
+            ifstream infile(PROB + ".in");
+            if (infile.good()) {
+                freopen((PROB + ".in").c_str(), "r", stdin);
+                freopen((PROB + ".out").c_str(), "w", stdout);
+            }
+        }
+    }
+}
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void prD(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x);
+    template<class T> void prD(const vector<T>& x);
+    template<class T> void prD(const set<T>& x);
+    template<class T1, class T2> void prD(const map<T1,T2>& x);
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void prD(const T& x) { cout << x; }
+    template<class Arg, class... Args> void prD(const Arg& first, const Args&... rest) {
+        prD(first); prD(rest...);
+    }
+
+    template<class T1, class T2> void prD(const pair<T1,T2>& x) {
+        prD("{",x.pA,", ",x.pB,"}");
+    }
+    template<class T> void prDContain(const T& x) {
+        prD("{");
+        bool fst = 1; for (const auto& a: x) prD(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        prD("}");
+    }
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x) { prDContain(x); }
+    template<class T> void prD(const vector<T>& x) { prDContain(x); }
+    template<class T> void prD(const set<T>& x) { prDContain(x); }
+    template<class T1, class T2> void prD(const map<T1,T2>& x) { prDContain(x); }
+
+    void psD() { prD("\n"); }
+    template<class Arg> void psD(const Arg& first) {
+        prD(first); psD(); // no space at end of line
+    }
+    template<class Arg, class... Args> void psD(const Arg& first, const Args&... rest) {
+        prD(first," "); psD(rest...); // print w/ spaces
+    }
+
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+        pr(first); pr(rest...);
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+        pr(x.pA, " ", x.pB);
+    }
+    template<class T> void prContain(const T& x) {
+        bool fst = 1; for (const auto& a: x) pr(!fst?" ":"",a), fst = 0; // const needed for vector<bool>
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) {
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+using namespace output;
+// @formatter:on
+
+/* ============================ */
+
+int n, m, k, q;
+int leftTreasure[200000], rightTreasure[200000];
+int exitCol[200000][4];
+int highestRowWithTreasure = 0;
+
+ll go(int pos, int row, int dest) {
+    int best = INF;
+    int l = leftTreasure[row], r = rightTreasure[row];
+
+    if (l == INF) return abs(dest - pos);
+
+    if (pos >= l && pos <= r) {
+        MIN(best, (r - pos) + (r - l) + (dest == -1 ? 0 : abs(dest - l)));
+        MIN(best, (pos - l) + (r - l) + (dest == -1 ? 0 : abs(dest - r)));
+    } else if (pos > r) {
+        best = (pos - l) + (dest == -1 ? 0 : abs(dest - l));
+    } else if (pos < l) {
+        best = (r - pos) + (dest == -1 ? 0 : abs(dest - r));
+    } else {
+        assert(false);
+    }
+    return best;
+}
+
+ll memo[200000][4];
+ll dp(int row, int exitC) {
+    if (memo[row][exitC] != -1) return memo[row][exitC];
+
+    int pos;
+    if (row == 0) {
+        pos = 0;
+    } else {
+        pos = exitCol[row][exitC];
+    }
+
+    if (row >= highestRowWithTreasure) {
+        return go(pos, row, -1);
+    }
+
+    ll best = LL_INF;
+
+    F0R(i, 4) {
+        if (exitCol[row + 1][i] != -1) {
+            MIN(best, go(pos, row, exitCol[row + 1][i]) + dp(row + 1, i) + 1);
+        }
+    }
+    assert(best != LL_INF);
+
+    return memo[row][exitC] = best;
+}
+
+int main() {
+    setupIO();
+
+    re(n, m, k, q);
+    SET(leftTreasure, INF, n); SET(rightTreasure, -1, n);
+    F0R(i, k) {
+        int r, c; re(r, c); r--; c--;
+        MIN(leftTreasure[r], c);
+        MAX(rightTreasure[r], c);
+        MAX(highestRowWithTreasure, r);
+    }
+    set<int> safeCols;
+    F0R(i, q) {
+        int x; re(x); x--;
+        safeCols.insert(x);
+    }
+    SET2D(exitCol, -1, n, 4);
+    F0R(i, n) {
+        if (leftTreasure[i] == INF) {
+            // no treasure on this row, exit col is same as last row
+            if (i == 0) {
+                // except in this case :P
+                exitCol[i][0] = *safeCols.begin();
+            } else {
+                F0R(j, 4) exitCol[i][j] = exitCol[i - 1][j];
+            }
+        } else {
+            int l = leftTreasure[i], r = rightTreasure[i];
+            auto it = safeCols.ub(l);
+            if (it != safeCols.begin()) {
+                exitCol[i][0] = *--it;
+            }
+            it = safeCols.lb(l);
+            if (it != safeCols.end()) {
+                exitCol[i][1] = *it;
+            }
+
+            it = safeCols.ub(r);
+            if (it != safeCols.begin()) {
+                exitCol[i][2] = *--it;
+            }
+            it = safeCols.lb(r);
+            if (it != safeCols.end()) {
+                exitCol[i][3] = *it;
+            }
+        }
+    }
+
+    SET2D(memo, -1, n, 4);
+    ps(dp(0, 0));
+
+    return 0;
+}
