ecf5356677dfc9b929149084be0e6de6865b1844
Nathan Wang <nathan.r.wang@gmail.com>
2020-01-26 01:53:58 -0800

    cave 2020jan plat

---
+++ USACO/2020jan/plat/cave.cpp
@@ -0,0 +1,263 @@
+/*
+ * Process the painting from bottom right corner to top left corner.
+ *
+ * Use a Union Find data structure to merge the caves. I believe my solution is the same as the editorial
+ */
+
+//#pragma GCC optimize ("O3")
+//#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F0R1(i, a) for (int i=1; i<=(a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define MIN(a, b) a = min(a, b)
+#define MAX(a, b) a = max(a, b)
+
+#define INF 1000000010
+#define LL_INF 4500000000000000000LL
+#define LSOne(S) (S & (-S))
+#define EPS 1e-9
+#define pA first
+#define pB second
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define PI acos(-1.0)
+// #define MOD (int)(2e+9+11)
+#define MOD (int)(1e+9+7)
+#define SET(vec, val, size) for (int i = 0; i < size; i++) vec[i] = val;
+#define SET2D(arr, val, dim1, dim2) F0R(i, dim1) F0R(j, dim2) arr[i][j] = val;
+#define SET3D(arr, val, dim1, dim2, dim3) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) arr[i][j][k] = val;
+#define SET4D(arr, val, dim1, dim2, dim3, dim4) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) F0R(l, dim4) arr[i][j][k][l] = val;
+
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+#define SORT(vec) sort(all(vec))
+#define RSORT(vec) sort(vec.rbegin(),vec.rend())
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<ll> vl;
+
+// @formatter:off
+// Source: Benq (https://github.com/bqi343/USACO) [Modified]
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+    template<class T> void reA(T A[], int sz);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) {
+        re(first); re(rest...);
+    }
+
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.pA,p.pB); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+    template<class T> void reA(T A[], int sz) { F0R(i, sz) re(A[i]); }
+
+    void setupIO(const string &PROB = "") {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        if (PROB.length() != 0) {
+            ifstream infile(PROB + ".in");
+            if (infile.good()) {
+                freopen((PROB + ".in").c_str(), "r", stdin);
+                freopen((PROB + ".out").c_str(), "w", stdout);
+            }
+        }
+    }
+}
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void prD(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x);
+    template<class T> void prD(const vector<T>& x);
+    template<class T> void prD(const set<T>& x);
+    template<class T1, class T2> void prD(const map<T1,T2>& x);
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void prD(const T& x) { cout << x; cout.flush(); }
+    template<class Arg, class... Args> void prD(const Arg& first, const Args&... rest) {
+        prD(first); prD(rest...);
+    }
+
+    template<class T1, class T2> void prD(const pair<T1,T2>& x) {
+        prD("{",x.pA,", ",x.pB,"}");
+    }
+    template<class T> void prDContain(const T& x) {
+        prD("{");
+        bool fst = 1; for (const auto& a: x) prD(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        prD("}");
+    }
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x) { prDContain(x); }
+    template<class T> void prD(const vector<T>& x) { prDContain(x); }
+    template<class T> void prD(const set<T>& x) { prDContain(x); }
+    template<class T1, class T2> void prD(const map<T1,T2>& x) { prDContain(x); }
+
+    void psD() { prD("\n"); }
+    template<class Arg> void psD(const Arg& first) {
+        prD(first); psD(); // no space at end of line
+    }
+    template<class Arg, class... Args> void psD(const Arg& first, const Args&... rest) {
+        prD(first," "); psD(rest...); // print w/ spaces
+    }
+
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+        pr(first); pr(rest...);
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+        pr(x.pA, " ", x.pB);
+    }
+    template<class T> void prContain(const T& x) {
+        bool fst = 1; for (const auto& a: x) pr(!fst?" ":"",a), fst = 0; // const needed for vector<bool>
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) {
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+using namespace output;
+// @formatter:on
+
+/* ============================ */
+
+int n, m;
+char A[1000][1000];
+int height[1000000];
+int height2[1000000];
+ull ans[1000000];
+
+class UnionFind {
+private:
+    vi p;
+public:
+    UnionFind(int N) {
+        p.assign(N, 0);
+        for (int i = 0; i < N; i++) p[i] = i;
+    }
+
+    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+
+    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+
+    void unionSet(int i, int j) {
+        if (!isSameSet(i, j)) {
+            int x = findSet(i), y = findSet(j);
+            p[y] = x;
+        }
+    }
+};
+
+int zip(int i, int j) {
+    return i * m + j;
+}
+
+int main() {
+    setupIO("cave");
+
+    re(n, m);
+    F0R(i, n) {
+        F0R(j, m) {
+            re(A[i][j]);
+            height[zip(i, j)] = INF;
+            height2[zip(i, j)] = INF;
+        }
+    }
+
+    UnionFind UF(n * m);
+
+    SET(ans, 1, n * m);
+
+    F0Rd(i, n) {
+        F0Rd(j, m) {
+            if (A[i][j] != '.') continue;
+
+            int a = i, b = j + 1;
+            if (b < m && A[a][b] == '.') {
+                UF.unionSet(zip(a, b), zip(i, j));
+            }
+
+            int x = UF.findSet(zip(i, j));
+
+            a = i + 1, b = j;
+            if (a < n && A[a][b] == '.') {
+                int y = UF.findSet(zip(a, b));
+                if (height[y] != i) {
+                    ans[x] = (ans[x] * ans[y]) % MOD;
+                    UF.unionSet(zip(i, j), zip(a, b));
+                } else if (x != y) {
+                    UF.unionSet(zip(a, b), zip(i, j));
+                    ans[UF.findSet(zip(a, b))] = (ans[UF.findSet(zip(a, b))]*ans[x])%MOD;
+                }
+            }
+
+            height[UF.findSet(zip(i, j))] = i;
+        }
+        F0Rd(j, m) {
+            if (A[i][j] == '.') {
+                int x = UF.findSet(zip(i, j));
+                if (height2[x] > i) {
+                    height2[x] = i;
+                    ans[x]++;
+                }
+            }
+        }
+    }
+
+    ull totAns = 1;
+    F0R(i, n) {
+        F0R(j, m) {
+            if (A[i][j] == '.' && zip(i, j) == UF.findSet(zip(i, j))) {
+                totAns = (totAns*ans[zip(i, j)])%MOD;
+                ans[zip(i, j)] = 1;
+            }
+        }
+    }
+    ps(totAns);
+
+    return 0;
+}
