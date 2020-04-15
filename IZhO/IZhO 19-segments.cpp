757f5503053fdd437fa56ef3f020c1cc8d2b4680
Nathan Wang <nathan.r.wang@gmail.com>
2020-04-15 16:39:22 -0700

    izho 19-segments

---
+++ IZhO/IZhO 19-segments.cpp
@@ -0,0 +1,225 @@
+/*
+ * Define dp[i] = { a, b } where a = the sum of the last segment that ends at i, b = the number of segments
+ *
+ * The base case is dp[i] = { sum of A[0...i], 1 } (basically one giant segment that ends in i)
+ *
+ * Transition: We try to see if there exists some j where dp[j].first <= sum of A[j+1...i] and j < i.
+ * If there is, we pick the largest such j, and we build a segment [j+1...i]:
+ * dp[i] = { sum of A[j+1...i], dp[j].second + 1 }
+ *
+ * This is n^2. To optimize down to O(n), we just optimize the transition.
+ * Begin by re-defining dp[i] to be { (sum of last segment ending at i) - (sum of A[i+1...n-1]), b }
+ * So we just subtract A[i+1...n-1] from every dp[i].first
+ *
+ * With this new definition, our transition is now "find the largest j such that DP[j] <= A[i...n-1]"
+ *
+ * We need a data structure that can do two things:
+ * 1. Add (k, v)
+ * 2. Query q: Return the largest index k such that the value v associated with k is <= q.
+ *
+ * We can implement this data structure with a vector of pairs that has the property that v is strictly
+ * increasing as k increases.
+ *
+ * To query, we just do a binary search on this vector.
+ */
+
+//#pragma GCC optimize ("O3")
+//#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define F0R1(i, a) for (int i=1; i<=(a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define MIN(a, b) a = min(a, b)
+#define MAX(a, b) a = max(a, b)
+
+#define INF 1000000010
+#define LL_INF 4500000000000000000LL
+#define LSOne(S) (S & (-S))
+#define EPS 1e-9
+#define pA first
+#define pB second
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define PI acos(-1.0)
+#define MOD2 (int)(2e+9+11)
+#define MOD (int)(1e+9+7)
+#define SET(vec, val, size) for (int i = 0; i < size; i++) vec[i] = val;
+#define SET2D(arr, val, dim1, dim2) F0R(i, dim1) F0R(j, dim2) arr[i][j] = val;
+#define SET3D(arr, val, dim1, dim2, dim3) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) arr[i][j][k] = val;
+#define SET4D(arr, val, dim1, dim2, dim3, dim4) F0R(i, dim1) F0R(j, dim2) F0R(k, dim3) F0R(l, dim4) arr[i][j][k][l] = val;
+
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+#define SORT(vec) sort(all(vec))
+#define RSORT(vec) sort(vec.rbegin(),vec.rend())
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<ll> vl;
+
+// @formatter:off
+// Source: Benq (https://github.com/bqi343/USACO) [Modified]
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+    template<class T> void reA(T A[], int sz);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) {
+        re(first); re(rest...);
+    }
+
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.pA,p.pB); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+    template<class T> void reA(T A[], int sz) { F0R(i, sz) re(A[i]); }
+
+    void setupIO(const string &PROB = "") {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        if (PROB.length() != 0) {
+            ifstream infile(PROB + ".in");
+            if (infile.good()) {
+                freopen((PROB + ".in").c_str(), "r", stdin);
+                freopen((PROB + ".out").c_str(), "w", stdout);
+            }
+        }
+    }
+}
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void prD(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x);
+    template<class T> void prD(const vector<T>& x);
+    template<class T> void prD(const set<T>& x);
+    template<class T1, class T2> void prD(const map<T1,T2>& x);
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void prD(const T& x) { cout << x; cout.flush(); }
+    template<class Arg, class... Args> void prD(const Arg& first, const Args&... rest) {
+        prD(first); prD(rest...);
+    }
+
+    template<class T1, class T2> void prD(const pair<T1,T2>& x) {
+        prD("{",x.pA,", ",x.pB,"}");
+    }
+    template<class T> void prDContain(const T& x) {
+        prD("{");
+        bool fst = 1; for (const auto& a: x) prD(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        prD("}");
+    }
+    template<class T, size_t SZ> void prD(const array<T,SZ>& x) { prDContain(x); }
+    template<class T> void prD(const vector<T>& x) { prDContain(x); }
+    template<class T> void prD(const set<T>& x) { prDContain(x); }
+    template<class T1, class T2> void prD(const map<T1,T2>& x) { prDContain(x); }
+
+    void psD() { prD("\n"); }
+    template<class Arg> void psD(const Arg& first) {
+        prD(first); psD(); // no space at end of line
+    }
+    template<class Arg, class... Args> void psD(const Arg& first, const Args&... rest) {
+        prD(first," "); psD(rest...); // print w/ spaces
+    }
+
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+        pr(first); pr(rest...);
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+        pr(x.pA, " ", x.pB);
+    }
+    template<class T> void prContain(const T& x) {
+        bool fst = 1; for (const auto& a: x) pr(!fst?" ":"",a), fst = 0; // const needed for vector<bool>
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) {
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+using namespace output;
+// @formatter:on
+
+/* ============================ */
+
+vector<pair<ll, int>> DS;
+void add(int key, ll value) {
+    while (sz(DS) > 0 && DS.back().pA >= value) DS.pop_back();
+    DS.pb({value, key});
+}
+int query(ll qry) {
+    auto it = lb(all(DS), mp(qry+1, -1)); --it;
+    return it->pB;
+}
+
+int main() {
+    setupIO();
+
+    int n; re(n);
+    vi A; A.resz(n); re(A);
+    vl PS; PS.resz(n);
+    vl PS2; PS2.resz(n+1);
+    PS[0] = A[0];
+    FOR(i, 1, n) PS[i] = PS[i-1] + A[i];
+    PS2[n] = 0;
+    F0Rd(i, n) PS2[i] = PS2[i+1] + A[i];
+
+    add(-1, -LL_INF);
+
+    pair<ll, int> dp[n];
+    F0R(i, n) {
+        dp[i] = { PS[i] - PS2[i+1], 1 };
+
+        int prev = query(-PS2[i+1]);
+        if (prev != -1) {
+            dp[i] = { PS[i] - PS[prev] - PS2[i+1], dp[prev].pB + 1 };
+        }
+        
+        add(i, dp[i].pA);
+    }
+
+    ps(dp[n-1].pB);
+}
+
