6d84bf308752d8562a16db0571c3cb345ad976c9
Nathan Wang <nathan.r.wang@gmail.com>
2020-06-27 18:38:50 -0700

    cleanup repo

--- ineffable/install.sh
+++
@@ -1,8 +0,0 @@
-mkdir ~/.ineffable
-cd ~/.ineffable
-wget https://bitbucket.org/silap/ineffable/downloads/ineffable-1.0.zip
-unzip master.zip
-rm master.zip
-mv silap-ineffable-*/* .
-rm -r silap-ineffable-*/
-sudo ln -s ~/.ineffable/ineffable /usr/local/bin/ineffable
