a81e37d067840e092711ee3a2b822d205fbcc32c
Nathan Wang <nathan.r.wang@gmail.com>
2020-10-29 21:27:51 -0700

    update solution notes

--- POI/POI 06-Periods.cpp
+++ POI/POI 06-Periods.cpp
@@ -5,12 +5,12 @@
  *   except we exclude the suffix X. (The length of X must be less than or equal to half the length of A.)
  * - So for babababa, since "ba" is both a prefix and a suffix, we chop off the "ba" suffix and "bababa" is a valid period.
  * - To maximize the length of the period, we need to minimize the length of the prefix-suffix X.
- * - Therefore, we just need a way to find, for every prefix of the given string, we need to find the minimum length of a prefix-suffix.
+ * - Therefore, we just need a way to find, for every prefix of the given string, the minimum length of a prefix-suffix.
  *   (If no such prefix-suffix exists, then there is no period for that prefix.)
  * - We can do this with a variation of the KMP string matching algorithm.
  * - In particular, we need to generate the prefix function array: https://cp-algorithms.com/string/prefix-function.html
- * - The prefix function pi[i] = longest prefix of given_string[0...i] which is also a suffix of this substring.
- *   - We can compute this in O(1) time; see the cp-algorithms link.
+ * - The prefix function pi[i] = longest prefix of the substring s[0...i] which is also a suffix of this substring.
+ *   - We can compute this in O(n) time; see the cp-algorithms link.
  * - Next, to get the smallest possible prefix-suffix, notice that if X is a prefix-suffix, then a prefix-suffix
  *   of X is also a valid prefix-suffix of the entire substring.
  * - So, after finding pi[i], we then check pi[pi[i]], then pi[pi[pi[i]]], and so on (note that you may need to
