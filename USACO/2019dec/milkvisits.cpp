fd16ae8cb0bdb54bccfb141ccc734daa90cfe468
Nathan Wang <nathan.r.wang@gmail.com>
2020-10-30 23:17:45 -0700

    milkvisits

---
+++ USACO/2019dec/milkvisits.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define ii pair<int, int>
+#define f first
+#define s second
+#define mp make_pair
+
+int n, m; 
+int A[100000];
+vector<int> adj[100000];
+bool ans[100000];
+vector<pair<int, ii>> queries[100000];
+
+// [in, out)
+int in[100000], out[100000], depth[100000];
+int timer = 0;
+void dfs2(int u, int p, int d = 0) {
+    in[u] = timer++;
+    depth[u] = d;
+    for (int v : adj[u]) {
+        if (v != p) dfs2(v, u, d+1);
+    }
+    out[u] = timer;
+}
+
+stack<int> nodes[100000];
+vector<int> s;
+void dfs(int u, int p) {
+    s.pb(u);
+    nodes[A[u]].push(u);
+    for (auto q : queries[u]) {
+        int y = q.f, c = q.s.f;
+        if (nodes[c].size() == 0) continue;
+        int tgt = nodes[c].top();
+
+        // check if tgt is on the path from u to y
+        bool good = false;
+        if (!(in[tgt] <= in[y] && out[tgt] > in[y])) good = true;
+        else if (depth[tgt]+1 < (int)s.size()) {
+            int nextChild = s[depth[tgt]+1];
+            if (!(in[nextChild] <= in[y] && out[nextChild] > in[y])) good = true;
+        }
+        if (tgt == y || tgt == u) good = true;
+
+        if (good) ans[q.s.s] = true;
+    }
+    for (int v : adj[u]) {
+        if (v == p) continue;
+        dfs(v, u);
+    }
+    nodes[A[u]].pop();
+    s.pop_back();
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("milkvisits.in", "r", stdin);
+    freopen("milkvisits.out", "w", stdout);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+        --A[i];
+    }
+    for (int i = 0; i < n-1; i++) {
+        int a, b; cin >> a >> b; --a; --b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c; --a; --b; --c;
+        queries[a].pb(mp(b, mp(c, i)));
+        queries[b].pb(mp(a, mp(c, i)));
+    }
+
+    dfs2(0, 0);
+    dfs(0, 0);
+
+    for (int i = 0; i < m; i++) {
+        cout << ans[i];
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ USACO/2019dec/milkvisits_2.cpp
@@ -0,0 +1,103 @@
+/*
+ * Solved w/ euler tour + compressed BIT
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define ii pair<int, int>
+#define f first
+#define s second
+#define mp make_pair
+
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T> using Tree = tree<T, null_type, less<T>, 
+	rb_tree_tag, tree_order_statistics_node_update>; 
+
+int n, m; 
+int A[100000];
+vector<int> adj[100000];
+
+Tree<int> add[100000], sub[100000];
+
+int pa[18][100000], depth[100000];
+
+// [in, out)
+int in[100000];
+int timer = 0;
+void dfs2(int u, int p, int d = 0) {
+    pa[0][u] = p;
+    depth[u] = d;
+    in[u] = timer;
+    add[A[u]].insert(timer);
+    timer++;
+    for (int v : adj[u]) {
+        if (v != p) {
+            dfs2(v, u, d+1);
+        }
+    }
+    sub[A[u]].insert(timer);
+    timer++;
+}
+
+int qry(int k, int x) {
+    x = in[x]+1;
+    int v = add[k].order_of_key(x) - sub[k].order_of_key(x);
+    assert(v >= 0);
+    return v;
+}
+
+int lca(int a, int b) {
+    if (depth[a] < depth[b]) swap(a, b);
+    for (int i = 17; i >= 0; i--) {
+        if (depth[pa[i][a]] >= depth[b]) a = pa[i][a];
+    }
+    if (a == b) return a;
+    for (int i = 17; ~i; i--) {
+        if (pa[i][a] != pa[i][b]) {
+            a = pa[i][a];
+            b = pa[i][b];
+        }
+    }
+    return pa[0][a];
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("milkvisits.in", "r", stdin);
+    freopen("milkvisits.out", "w", stdout);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+        --A[i];
+    }
+    for (int i = 0; i < n-1; i++) {
+        int a, b; cin >> a >> b; --a; --b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+
+    dfs2(0, 0);
+    for (int i = 1; i < 18; i++) {
+        for (int j = 0; j < n; j++) {
+            pa[i][j] = pa[i-1][pa[i-1][j]];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c; --a; --b; --c;
+        if (qry(c, a) + qry(c, b) - 2*qry(c, lca(a, b)) > 0 || A[lca(a, b)] == c) {
+            cout << 1;
+        } else {
+            cout << 0;
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
