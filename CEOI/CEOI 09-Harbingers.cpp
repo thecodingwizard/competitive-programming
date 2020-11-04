95e96604f46ac17e2d03b0ace4b6042b646239b9
Nathan Wang <nathan.r.wang@gmail.com>
2020-11-03 16:23:22 -0800

    wip

---
+++ CEOI/CEOI 09-Harbingers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define ii pair<int, int>
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define F0R(i, n) for (int i = 0; i < n; i++)
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define inf 1000000010
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n; cin >> n;
+
+    return 0;
+}
